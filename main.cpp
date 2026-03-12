1eb79b987eaf31f3dbbdf02bc8d9f83bc241431a
John <dazajohn08@gmail.com>
2026-03-11 20:35:27 -0700

    fixed

--- main.cpp
+++ main.cpp
@@ -8,16 +8,21 @@ int main()
   const double ClassCCost = 9.00;
 
   // TODO: Declare integer variables for tickets sold for each class
+  
   int ticketsA, ticketsB, ticketsC;
   // TODO: Declare double variables for total sales per class and grand total
+  
   double totalA, totalB, totalC, total;
   // TODO: Prompt user and read number of Class A tickets sold
+  
   cout << "Enter number of Class A Tickets sold: ";
   cin >> ticketsA;
   // TODO: Prompt user and read number of Class B tickets sold
+  
   cout << "Enter number of Class B Tickets sold: ";
   cin >> ticketsB;
   // TODO: Prompt user and read number of Class C tickets sold
+  
   cout << "Enter number of Class C Tickets sold: ";
   cin >> ticketsC;
   // TODO: Calculate sales for each class
