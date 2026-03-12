7250e081e26fa1a9cdbcf99f65a529b998f89706
John <dazajohn08@gmail.com>
2026-03-11 20:32:52 -0700

    Finished

--- main.cpp
+++ main.cpp
@@ -8,18 +8,26 @@ int main()
   const double ClassCCost = 9.00;
 
   // TODO: Declare integer variables for tickets sold for each class
-
+  int ticketsA, ticketsB, ticketsC;
   // TODO: Declare double variables for total sales per class and grand total
-
+  double totalA, totalB, totalC, total;
   // TODO: Prompt user and read number of Class A tickets sold
-
+  cout << "Enter number of Class A Tickets sold: ";
+  cin >> ticketsA;
   // TODO: Prompt user and read number of Class B tickets sold
-
+  cout << "Enter number of Class B Tickets sold: ";
+  cin >> ticketsB;
   // TODO: Prompt user and read number of Class C tickets sold
-
+  cout << "Enter number of Class C Tickets sold: ";
+  cin >> ticketsC;
   // TODO: Calculate sales for each class
+  totalA = ticketsA * ClassACost;
+  totalB = ticketsB * ClassBCost;
+  totalC = ticketsC * ClassCCost;
+
 
   // TODO: Calculate total sales
+  total = totalA + totalB + totalC;
 
   cout << setprecision(2) << fixed;
   cout << "Tickets sold for Class A " << ticketsA << " Sales Price for A   " << totalA << endl;
